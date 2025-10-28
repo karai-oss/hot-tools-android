af846989541bc5579b09513fdaf047bd71a67857
karai <karai@txzing.com>
2025-10-28 18:41:32 +0800

    feat:新增

--- hot-tools/src/main/java/com/xy/hottools/core/ViewUtils.java
+++ hot-tools/src/main/java/com/xy/hottools/core/ViewUtils.java
@@ -1,4 +1,49 @@
 package com.xy.hottools.core;
 
+import static androidx.constraintlayout.widget.ConstraintSet.VISIBLE;
+
+import android.view.View;
+
+/**
+ * View工具类
+ *
+ * @author Mr.xie
+ * @apiNote V1
+ */
 public class ViewUtils {
+
+    /**
+     * View可见
+     *
+     * @param views 多个view
+     */
+    public static void visible(View... views) {
+        for (View v : views) {
+            v.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * View不可见
+     *
+     * @param views 多个view
+     */
+    public static void hide(View... views) {
+        for (View v : views) {
+            v.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * 显示/隐藏View
+     *
+     * @param visibleView 需要显示的View
+     * @param hideView    需要隐藏的View
+     */
+    public static void visibility(View visibleView, View hideView) {
+        if (visibleView == null) return;
+        if (hideView == null) return;
+        visibleView.setVisibility(View.VISIBLE);
+        hideView.setVisibility(View.GONE);
+    }
 }
